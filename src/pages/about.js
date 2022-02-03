c2e37d1db8e572055130873b3e8d22f68dc7a2ca
Kellouch <younes.kellouch@gmail.com>
2022-02-03 17:21:12 +0100

    set uo the new project

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import { Link } from "gatsby"
+import React from "react"
+
+const about = () => {
+  return (
+    <>
+      <div> About Page</div>
+      <Link to="/">Home</Link>
+    </>
+  )
+}
+
+export default about
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
+import { Link } from "gatsby"
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <section>
+      <div>
+        <h1>hey it's the first project we test here</h1>
+        <h2>My name is younes and i'll be a millionare</h2>
+        <p>good bye</p>
+        <Link to="/about">About page</Link>
+      </div>
+    </section>
+  )
 }
