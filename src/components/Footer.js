18ab433f20d08adcb0fd449f3f414e0013796e87
Kellouch <younes.kellouch@gmail.com>
2022-02-04 14:57:43 +0100

    updating all files to start the real project

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
 import React from "react"
 
 const Footer = () => {
-  return <footer> Welcome from footer</footer>
+  return (
+    <footer className="page-footer">
+      <p>
+        &copy; {new Date().getFullYear()} <span>SimpleRecipes</span>
+      </p>{" "}
+      Built with <a href="https://www.gatsbyjs.com/">Gatsby</a>
+    </footer>
+  )
 }
 
 export default Footer
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,32 @@
 import { Link } from "gatsby"
 import React from "react"
+import { FiMenu } from "react-icons/fi"
+import logo from "../assets/images/logo.svg"
 
 const Navbar = () => {
-  return <nav>
-      <ul>
-          <li>
-              <Link to="/">home</Link>
-          </li>
-          <li>
-              <Link to="/about">about</Link>
-          </li>
-          <li>
-              <Link to="/contact">contact</Link>
-          </li>
-          <li>
-              <Link to="/tags">tags</Link>
-          </li>
-          <li>
-              <Link to="/recipes">recipes</Link>
-          </li>
-      </ul>
-  </nav>
+  return (
+    <nav className="navBar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="simply recipes" />
+          </Link>
+          <button className="nav-btn">
+            <FiMenu />
+          </button>
+        </div>
+        <div className="nav-links show-links">
+          <Link to="/" className="nav-link" activeClassName="active-link">home</Link>
+          <Link to="/recipes" className="nav-link" activeClassName="active-link">recipes</Link>
+          <Link to="/tags" className="nav-link" activeClassName="active-link">tags</Link>
+          <Link to="/about" className="nav-link" activeClassName="active-link">about</Link>
+          <div className="nav-link contact-link">
+            <Link to="/contact" className="btn"> contact</Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
