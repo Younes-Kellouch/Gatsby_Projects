2e12f2ed7cdd07757e164666bafc6c8c3c314fab
Kellouch <younes.kellouch@gmail.com>
2022-02-04 13:09:52 +0100

    Adding main.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,19 @@ const Navbar = () => {
   return <nav>
       <ul>
           <li>
-              <Link to="/">Home</Link>
+              <Link to="/">home</Link>
           </li>
           <li>
-              <Link to="/about">About</Link>
+              <Link to="/about">about</Link>
+          </li>
+          <li>
+              <Link to="/contact">contact</Link>
+          </li>
+          <li>
+              <Link to="/tags">tags</Link>
+          </li>
+          <li>
+              <Link to="/recipes">recipes</Link>
           </li>
       </ul>
   </nav>
