4fa23fb31a888b0edf5967a8563028cfd6660417
Kellouch <younes.kellouch@gmail.com>
2022-02-04 02:06:36 +0100

    Install the styled Component Plugin (Gatsby Plugin)

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,27 @@
 import { Link } from "gatsby"
 import React from "react"
+import styled from "styled-components"
 import Layouts from "../components/Layouts"
 
 const about = () => {
   return (
     <Layouts>
-      <div> About Page</div>
-      <Link to="/">Home</Link>
+      <Wrraper>
+        <div> About Page</div>
+        <Link to="/">Home</Link>
+      </Wrraper>
     </Layouts>
   )
 }
+
+const Wrraper = styled.section`
+  font-weight: bold;
+  color: pink;
+
+  a {
+    text-decoration: none;
+    color: green;
+  }
+`
 
 export default about
