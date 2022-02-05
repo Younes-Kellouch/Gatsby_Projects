0d3b75ae661f35e1d58864c3eaaccb963c164fd0
Kellouch <younes.kellouch@gmail.com>
2022-02-05 09:35:57 +0100

    Testing staticImage (Constrained/default)

--- src/Examples/Images.js
+++ src/Examples/Images.js
@@ -1,10 +1,36 @@
+import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
 import styled from "styled-components"
 
 const Images = () => {
-  return <div></div>
+  return (
+    <Wrraper>
+      <article>
+        <h4>constrained /default</h4>
+        <StaticImage
+          src="../assets/images/big.jpg"
+          alt="food"
+          height={400}
+          placeholder="tracedSVG"
+          layout="constrained"
+          className="example-image"
+          as="section"
+        />
+        <h1>Gatsby Images</h1>
+      </article>
+    </Wrraper>
+  )
 }
 
-const Wrraper = styled.section``
+const Wrraper = styled.section`
+    width: 70vw;
+    margin: 0 auto;
+    article{
+        border: 2px solid blue;
+    }
+    .example-image{
+        border-radius: 1rem;
+    }
+`
 
 export default Images
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layouts"
+import Images from "../Examples/Images"
 
 export default function Home() {
   return (
     <Layout>
       <h1>Home Page</h1>
+      <Images/>
     </Layout>
   )
 }
