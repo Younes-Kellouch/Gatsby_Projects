20a0535ed6227da964e9f8774615d3be4a968061
Kellouch <younes.kellouch@gmail.com>
2022-02-05 11:01:34 +0100

    Update the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,28 @@
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
 import Layout from "../components/Layouts"
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="eggs"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          />
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>simply recipes</h1>
+              <h4>no fluff, just recipes</h4>
+            </div>
+          </div>
+        </header>
+      </main>
     </Layout>
   )
 }
