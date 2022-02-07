8898a1405d9e8648005e1cc06a29e60cc64603b2
Kellouch <younes.kellouch@gmail.com>
2022-02-07 02:22:28 +0100

    Grabing the files with graphQL and displaying them in the browser

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,11 +1,38 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
+
+const query = graphql`
+  {
+    allFile(filter: { extension: { ne: "svg" } }) {
+      nodes {
+        name
+        childImageSharp {
+          gatsbyImageData(
+            layout: CONSTRAINED
+            placeholder: BLURRED
+            transformOptions: { grayscale: true }
+            width: 200
+          )
+        }
+      }
+    }
+  }
+`
 
 const Gallery = () => {
+  const data = useStaticQuery(query)
+  const nodes = data.allFile.nodes
   return (
-    <div>
-      <h2>Gallery Page</h2>
-    </div>
+    <Wrraper>
+      {nodes.map((image,index)=>{
+        console.log(image)
+        return(<article key={index}>{image.name}</article>)
+      })}
+    </Wrraper>
   )
 }
+
+const Wrraper = styled.section``
 
 export default Gallery
