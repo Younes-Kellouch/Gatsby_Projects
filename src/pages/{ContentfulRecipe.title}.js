8bb60d348132e75b84db92a8d0c9f39e105db6f3
Kellouch <younes.kellouch@gmail.com>
2022-02-08 13:11:46 +0100

    create a page that will provide us the recipes pages dynamically (slugs) but we still got problem accessing the pages from the links we set before

---
+++ src/pages/{ContentfulRecipe.title}.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layouts from "../components/Layouts"
+
+const RecipeTemplate = () => {
+  return (
+    <Layouts>
+      <h4>recipe template</h4>
+    </Layouts>
+  )
+}
+
+export default RecipeTemplate
