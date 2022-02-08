e24404fd7e9b28c3fbc4fe920710228145bc4449
Kellouch <younes.kellouch@gmail.com>
2022-02-08 12:30:11 +0100

    setUp the setUpTags function

--- src/components/tags/TagsList.js
+++ src/components/tags/TagsList.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import setUpTags from '../../utils/setUpTags';
 
 const TagsList = ({recipes}) => {
+  setUpTags(recipes)
   return <div>Tags List</div>;
 };
 
---
+++ src/utils/setUpTags.js
@@ -0,0 +1,27 @@
+const setUpTags = (recipes = []) => {
+  console.log("setUpTags")
+  const allTags = {}
+
+  recipes.forEach(recipe => {
+    recipe.content.tags.forEach(tag => {
+      if (allTags[tag]) allTags[tag] = allTags[tag] + 1
+      else allTags[tag] = 1
+    })
+  })
+  console.log("array : " ,Object.entries(allTags))
+  const newTags = Object.entries(allTags).sort((a,b)=>{
+      console.log("A :" +a )
+      console.log("B :" +b )
+      const [firstTag] = a
+      console.log("firstTag :" +firstTag )
+      const [secondTag] = b
+      console.log("secondTag :" +secondTag )
+
+      return firstTag.localeCompare(secondTag)
+  })
+
+  console.log(newTags)
+  return recipes
+}
+
+export default setUpTags
