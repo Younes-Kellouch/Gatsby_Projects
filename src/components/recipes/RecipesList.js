892aa93c2fa705c2c6b5a575da20936ede3c3f8c
Kellouch <younes.kellouch@gmail.com>
2022-02-08 01:39:20 +0100

    Displaying the futured recipes that we get back from the graphQL query in the about page

--- src/components/recipes/RecipesList.js
+++ src/components/recipes/RecipesList.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import { Link } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import React from "react"
 
-const RecipesList = ({recipes}) => {
-  return <div>Recipes List</div>;
-};
+const RecipesList = ({ recipes = [] }) => {
+  return (
+    <div className="recipes-list">
+      {recipes.map(recipe => {
+        const { id, title, cookTime, prepTime, image } = recipe
+        return (
+          <Link to={`/${title}`} key={id} className="recipe">
+            <GatsbyImage image={getImage(image)} alt={`${title}`} />
+            <h5>{title}</h5>
+            <p>Prep: {prepTime}min | Cook: {cookTime} min</p>
+          </Link>
+        )
+      })}
+    </div>
+  )
+}
 
-export default RecipesList;
+export default RecipesList
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
 import Layout from "../components/Layouts"
+import RecipesList from "../components/recipes/RecipesList"
 
-const About = () => {
+const About = ({ data:{allContentfulRecipe:{nodes:recipes}} }) => {
+  console.log(recipes)
   return (
     <Layout>
       <main className="page">
@@ -17,17 +19,47 @@ const About = () => {
             <p>
               Taiyaki tumblr flexitarian jean shorts brunch, aesthetic salvia
               retro.
-            </p> 
+            </p>
             <Link to="/contact" className="btn">
               contact
             </Link>
           </article>
 
-          <StaticImage src="../assets/images/about.jpeg" alt="salt in Bowl" className="about-img" />
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="salt in Bowl"
+            className="about-img"
+          />
+        </section>
+        <section className="featured-recipes">
+          <h5>Look at this Awesome souce!</h5>
+          <RecipesList recipes={recipes}/>
         </section>
       </main>
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allContentfulRecipe(
+      sort: { fields: title, order: ASC }
+      filter: { featured: { eq: true } }
+    ) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        content {
+          tags
+        }
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+      }
+    }
+  }
+`
 
 export default About
