0db3ca39e82441b8e9c9afc944ca2119137dec19
Kellouch <younes.kellouch@gmail.com>
2022-02-08 13:30:48 +0100

    install the slugify that let us transform our title to a valid text for the link so we can solve the prev problem of our links

--- src/components/recipes/RecipesList.js
+++ src/components/recipes/RecipesList.js
@@ -1,17 +1,25 @@
 import { Link } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import React from "react"
+import slugify from "slugify"
 
 const RecipesList = ({ recipes = [] }) => {
   return (
     <div className="recipes-list">
       {recipes.map(recipe => {
         const { id, title, cookTime, prepTime, image } = recipe
+        const slug = slugify(title, { lower: true })
         return (
-          <Link to={`/${title}`} key={id} className="recipe">
-            <GatsbyImage image={getImage(image)} alt={title} className="recipe-img" />
+          <Link to={`/${slug}`} key={id} className="recipe">
+            <GatsbyImage
+              image={getImage(image)}
+              alt={title}
+              className="recipe-img"
+            />
             <h5>{title}</h5>
-            <p>Prep: {prepTime}min | Cook: {cookTime} min</p>
+            <p>
+              Prep: {prepTime}min | Cook: {cookTime} min
+            </p>
           </Link>
         )
       })}
